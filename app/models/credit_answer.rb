2bcc295b4b9d3747b9874ef738ff42f5e60dcf1a
Nikesh <nikeshs129@gmail.com>
2023-11-06 22:05:20 +0530

    upload logic changed

--- app/models/credit_answer.rb
+++ app/models/credit_answer.rb
@@ -1,5 +1,7 @@
 class CreditAnswer < ApplicationRecord
-  belongs_to :credit_question, optional: true, dependent: :destroy
-  belongs_to :response, optional: true, dependent: :destroy
+  has_many_attached :file_upload
+  belongs_to :credit_question, optional: true
+  belongs_to :response, optional: true
+  belongs_to :credit_section, optional: true
   accepts_nested_attributes_for :credit_question
 end
