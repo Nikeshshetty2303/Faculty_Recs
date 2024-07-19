cc445b0123dcab687205e544cf88b36ad4eb86fa
Nikesh <nikeshs129@gmail.com>
2024-07-19 23:43:25 +0530

    Foreign Key Destroy Issue Resolved

--- app/models/credit_question.rb
+++ app/models/credit_question.rb
@@ -2,4 +2,5 @@ class CreditQuestion < ApplicationRecord
   belongs_to :credit_section, optional: true
   belongs_to :header, class_name: "CreditQuestion", foreign_key: "header_id", optional: true
   has_many :sub_questions, class_name: "CreditQuestion", foreign_key: "header_id", dependent: :destroy
+  has_many :credit_answers, dependent: :destroy
 end
