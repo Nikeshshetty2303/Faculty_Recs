3ea97ad7c23ebb981a45f33b28c7979c8c3dff57
Nikesh <nikeshs129@gmail.com>
2024-07-21 14:01:24 +0530

    Update on Confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_20_051202) do
+ActiveRecord::Schema.define(version: 2024_07_21_082959) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -207,6 +207,11 @@ ActiveRecord::Schema.define(version: 2024_07_20_051202) do
     t.integer "tab_no", default: 1
     t.integer "nav_tab_no", default: 1
     t.string "validator"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
