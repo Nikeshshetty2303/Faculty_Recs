72e2663bfb400ca96886071f53c534547a6c7483
Nikesh <nikeshs129@gmail.com>
2024-07-21 15:42:42 +0530

    rec5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.active_storage.service = :local
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'recruit.nitk.ac.in'}
+  config.action_mailer.default_url_options = { host: 'recruit.nitk.ac.in', port: 465 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_options = {from: 'crfnitk@gmail.com'}
