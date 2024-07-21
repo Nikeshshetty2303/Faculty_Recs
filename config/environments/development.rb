23eb714686baf26485321a7ef50606c614d24c93
Nikesh <nikeshs129@gmail.com>
2024-07-21 13:22:20 +0530

    Development file matched

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,8 +42,7 @@ Rails.application.configure do
   config.active_storage.service = :local
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-# config.action_mailer.default_url_options = { :host => "https://crfbeta.nitk.ac.in"  }
+  config.action_mailer.default_url_options = { :host => "recruit.nitk.ac.in"  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_options = {from: 'crfnitk@gmail.com'}
