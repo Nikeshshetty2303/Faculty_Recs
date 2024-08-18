7d08b0f326f51743a0b8a9944e5a88ab1bc58914
Nikesh <nikeshs129@gmail.com>
2024-08-18 10:36:37 +0530

    Minor Updates in the admin redirection

--- app/controllers/restriction_controller.rb
+++ app/controllers/restriction_controller.rb
@@ -1,6 +1,9 @@
 class RestrictionController < ApplicationController
     def frozen_application
         @user = User.find(current_user.id)
+        if @user.role == 'admin'
+          redirect_to admin_dashboard_all_responses_path(userid: @user.id)
+        end
         @responses = Response.where(user_id: @user.id)
     end
 
