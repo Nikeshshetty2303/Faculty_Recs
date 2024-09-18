91e61c25ce3c518d08e2ad04e56054bff060953a
Nikesh <nikeshs129@gmail.com>
2024-09-18 11:33:26 +0530

    Except converted into an array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,7 +38,7 @@ class HomeController < ApplicationController
     @user = User.find(current_user.id)
     @response = Response.all
     @status = false
-    @except = "EC"
+    @except = ["EC"]
   end
 
   def all_responses
