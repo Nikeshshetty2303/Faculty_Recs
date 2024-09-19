9d47f4e98b9a44d9b7d9fbeeacd04d4af06d6228
Nikesh <nikeshs129@gmail.com>
2024-09-19 16:34:09 +0530

    Opened For SOM Dept

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,7 +38,7 @@ class HomeController < ApplicationController
     @user = User.find(current_user.id)
     @response = Response.all
     @status = false
-    @except = ["MA", "CV"]
+    @except = ["MA", "CV", "SM"]
   end
 
   def all_responses
