bddbd7d94e242e0ee8c6a90ce80961d66314862a
Nikesh <nikeshs129@gmail.com>
2024-09-23 21:58:55 +0530

    IT and CS opened

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,7 +38,7 @@ class HomeController < ApplicationController
     @user = User.find(current_user.id)
     @response = Response.all
     @status = false
-    @except = ["MA", "ME"]
+    @except = ["MA", "ME","IT", "CS"]
   end
 
   def all_responses
