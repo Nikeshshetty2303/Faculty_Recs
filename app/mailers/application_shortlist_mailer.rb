6983e8f78433250a3d9bd5fe79f88bfb8c002783
Nikesh <nikeshs129@gmail.com>
2024-10-01 10:51:38 +0530

    Check

--- app/mailers/application_shortlist_mailer.rb
+++ app/mailers/application_shortlist_mailer.rb
@@ -13,18 +13,16 @@ class ApplicationShortlistMailer < ApplicationMailer
     end
 
     def referee
-        @user= User.find(params[:user_id])
+        @user = User.find(params[:user_id])
         @can_name = Answer.find(params[:can_name_id])
         @name = Answer.find(params[:ref_name_id])
         @email = Answer.find(params[:ref_email_id])
-        @ph_no = Anwser.find(params[:ref_ph_no_id])
-        @aff = Anwser.find(params[:ref_aff_no_id])
+        @ph_no = Answer.find(params[:ref_ph_no_id])
+        @aff = Answer.find(params[:ref_aff_id])
         mail(
-            from:"facrecruit@nitk.edu.in" ,
-            to: "#{@email}",
-            #cc: User.all.pluck(:email),
-            #bcc: "ok@gmail",
-            subject: "Letter of Reference"
-          )
+          from: "facrecruit@nitk.edu.in",
+          to: @email.content, # Assuming the email is stored in the 'content' attribute of the Answer
+          subject: "Letter of Reference"
+        )
     end
 end
