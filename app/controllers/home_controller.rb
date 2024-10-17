c6e85aa36d9d747d7a51cacd41fd0eb28fc44547
Nikesh <nikeshs129@gmail.com>
2024-10-17 10:37:42 +0530

    Validator Closed for All Department

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,7 +38,7 @@ class HomeController < ApplicationController
     @user = User.find(current_user.id)
     @response = Response.all
     @status = false
-    @except = ["EC"]
+    @except = []
   end
 
   def all_responses
